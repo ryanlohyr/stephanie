0e9eab47ee7a1c6d1c6083752d1f0454b47197f9
Ryan Loh <ryanloh29@gmail.com>
2024-08-09 22:54:48 -0700

    coddde

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import FlowerAnimation from "@/FlowerAnimation";
+
+export default function Home() {
+  return (
+    <main className="">
+      <div className="valentine_header text-white">
+        <img width="90" src="/stephaniee.png"></img>
+        <h1>I love you Stephanie You</h1>
+        <h1>七夕节快乐</h1>
+      </div>
+      <FlowerAnimation />
+    </main>
+  );
+}
